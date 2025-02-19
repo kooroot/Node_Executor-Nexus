4dbb05921ae315e66265c21a0657818482f6175d
kooroot <alswo9087@naver.com>
2025-02-19 12:49:39 +0900

    Update nexus_node.sh

--- nexus_node.sh
+++ nexus_node.sh
@@ -61,7 +61,6 @@ screen -S "$SESSION_NAME" -m bash -c '
     }
 
     rustup target add riscv32i-unknown-none-elf
-    cargo build
 
     echo "protobuf 수동 설치"
     https://github.com/protocolbuffers/protobuf/releases/download/v25.6/protoc-25.6-linux-x86_64.zip
